435f4d25a118eda0e94827c78b38ed1f6eff5028
Jon Michael Gomez <jmike2689@yahoo.com>
2026-08-09 11:37:01 -0500

    v1.8.3 - Native Android push notification and Firebase initialization hotfix

--- android/app/src/main/java/com/prosynctech/spritedex/MainActivity.java
+++ android/app/src/main/java/com/prosynctech/spritedex/MainActivity.java
@@ -1,5 +1,18 @@
 package com.prosynctech.spritedex;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.firebase.FirebaseApp;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        try {
+            FirebaseApp.initializeApp(this);
+        } catch (Exception e) {
+            // Safely catch if Firebase is already initialized
+        }
+    }
+}
